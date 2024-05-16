e03eede1f0f8b6e20a489114de62948f2af58448
Firemanhyeon <ghgus990129@naver.com>
2024-05-16 15:13:00 +0900

    05-16

--- springjdbc/src/main/java/org/example/springdatajdbc/MainApplication.java
+++ springjdbc/src/main/java/org/example/springdatajdbc/MainApplication.java
@@ -6,8 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.List;
 
 @SpringBootApplication
@@ -28,9 +32,34 @@ public class MainApplication {
 //
 //            User user1 = userRepository.findByName("john");
 //            System.out.println(user1.getName());
-            PageRequest pageRequest = PageRequest.of(1, 5);
-            Page<User> allUsersWithPagination = userRepository.findAllUsersWithPagination(pageRequest);
-            allUsersWithPagination.forEach(user -> System.out.println("name: "+user.getName()+" "+"email: "+user.getEmail()));
+//            PageRequest pageRequest = PageRequest.of(1, 5);
+//            Page<User> allUsersWithPagination = userRepository.findAllUsersWithPagination(pageRequest);
+//            allUsersWithPagination.forEach(user -> System.out.println("name: "+user.getName()+" "+"email: "+user.getEmail()));
+        };
+    }
+
+    @Bean
+    public CommandLineRunner batchUpdateDemo(JdbcTemplate jdbcTemplate){
+        return args -> {
+            List<User> users = Arrays.asList(
+                    new User(null, "Alice", "alice@example.com"),
+                    new User(null, "Bob", "bob@example.com"),
+                    new User(null, "Charlie", "charlie@example.com"),
+                    new User(null, "David", "david@example.com")
+            );
+
+            String sql = "INSERT INTO users (name, email) VALUES (?, ?)";
+
+            int[] updateCounts = jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
+                public void setValues(PreparedStatement ps, int i) throws  SQLException {
+                    User user = users.get(i);
+                    ps.setString(1,user.getName());
+                    ps.setString(2,user.getEmail());
+                }
+                public int getBatchSize() {
+                    return users.size();
+                }
+            });
         };
     }
 }
