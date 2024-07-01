5a0cffc9ee7400cce15032ca3c07edd23d96aa14
Firemanhyeon <ghgus990129@naver.com>
2024-07-01 21:21:48 +0900

    0701

---
+++ securityexam/src/main/java/org/blog/jwtexam/config/SecurityConfig.java
@@ -0,0 +1,63 @@
+package org.blog.jwtexam.config;
+
+import lombok.RequiredArgsConstructor;
+import org.blog.jwtexam.security.CustomUserDetailsService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final CustomUserDetailsService customUserDetailsService;
+
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/userregform","/userreg","/","/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+//                .formLogin(Customizer.withDefaults())
+                .formLogin(form -> form.disable())
+                .sessionManagement(sessionManagement -> sessionManagement
+//                        .sessionCreationPolicy(SessionCreationPolicy.ALWAYS) //스프링시큐리티가 항상세션생성
+//                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)//필요시 생성(기본값)
+//                        .sessionCreationPolicy(SessionCreationPolicy.NEVER)//생성하지는 않치만 이미 존재한다면 사용
+//                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)//생성하지도 않고, 존재해도 사용하지 않음 (JWT인증같은경우등)
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(csrf -> csrf.disable())
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()));
+
+        return http.build();
+    }
+    public CorsConfigurationSource corsConfigurationSource(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setAllowedMethods(List.of("GET" , "POST" , "DELETE" , "PATCH" , "OPTION" , "PUT"));
+        source.registerCorsConfiguration("/**",config);
+        return source;
+    }
+    //비밀번호 암호화해서 넣기
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ securityexam/src/main/java/org/blog/jwtexam/controller/UserApiController.java
@@ -0,0 +1,90 @@
+package org.blog.jwtexam.controller;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.blog.jwtexam.domain.RefreshToken;
+import org.blog.jwtexam.domain.Role;
+import org.blog.jwtexam.domain.User;
+import org.blog.jwtexam.dto.UserLoginResponseDto;
+import org.blog.jwtexam.jwt.util.JwtTokenizer;
+import org.blog.jwtexam.security.dto.UserLoginDto;
+import org.blog.jwtexam.service.RefreshTokenService;
+
+import org.blog.jwtexam.service.UserService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+public class UserApiController {
+
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenizer jwtTokenizer;
+    private final RefreshTokenService refreshTokenService;
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody @Valid UserLoginDto loginDto , BindingResult bindingResult , HttpServletResponse httpServletResponse){
+        //username , password가  null일때 혹은 정해진 형식에 맞지않을때
+        if(bindingResult.hasErrors()){
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+
+        //해당 username으로 유저정보 가져와서 아이디비밀번호 맞는지 체크 맞지않다면 return
+        User user = userService.findByUserName(loginDto.getUsername());
+        if(!passwordEncoder.matches(loginDto.getPassword(), user.getPassword())){
+            return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+        }
+        //여기까지체크한건 아이디비밀번호 일치하는지 체크. 아이디비밀번호맞음.
+
+        List<String> roles = user.getRoles().stream().map(Role::getName).collect(Collectors.toList()); //Role타입의 set에서 role의 이름만 들어있는 list가 필요함
+
+        //액세스토큰과 리프레시토큰 생성
+        String accessToken = jwtTokenizer.createAccessToken(user.getId(),user.getEmail(),user.getName(),user.getUsername(),roles);
+        String refreshToken = jwtTokenizer.createRefreshToken(user.getId(),user.getEmail(),user.getName(),user.getUsername(),roles);
+
+        //리프레시토큰 같은 경우엔 디비에 넣어줘야하니까 객체생성해서 넣어줌.
+        RefreshToken refreshTokenEntity = new RefreshToken();
+        refreshTokenEntity.setValue(refreshToken);
+        refreshTokenEntity.setUserId(user.getId());
+        refreshTokenService.addRefreshToken(refreshTokenEntity);
+
+        //응답을 위한 dto에 로그인정보 담아주기
+        UserLoginResponseDto loginResponse = UserLoginResponseDto.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .userId(user.getId())
+                .name(user.getName())
+                .build();
+
+        //쿠키에 액세스토큰 담아주기
+        Cookie accessTokenCookie = new Cookie("accessToken",accessToken);
+        accessTokenCookie.setHttpOnly(true);
+        accessTokenCookie.setPath("/");
+        accessTokenCookie.setMaxAge(Math.toIntExact(JwtTokenizer.ACCESS_TOKEN_EXPIRE_COUNT/1000));//30분 30분이 다되면 refresh토큰 가지고 다시 요청해서 맞으면 새로운 accesstoken 발급해준다.
+
+        //쿠키에 리프레시토큰 담아주기
+        Cookie refreshTokenCookie = new Cookie("refreshToken" , refreshToken);
+        refreshTokenCookie.setHttpOnly(true);//보안. (쿠키값을 js곳에서 접근할 수 없다.)
+        refreshTokenCookie.setPath("/");
+        refreshTokenCookie.setMaxAge(Math.toIntExact(JwtTokenizer.REFRESH_TOKEN_EXPIRE_COUT/1000));//유지시간. 7일 / 쿠키의 유지시간은 초단위인데 JWT의 시간단위는 밀리세컨드라 나눠줌.
+
+        //서블릿응답에 보내기
+        httpServletResponse.addCookie(accessTokenCookie);
+        httpServletResponse.addCookie(refreshTokenCookie);
+
+        //엔티티에 로그인정보담아서 return
+        return new ResponseEntity(loginResponse , HttpStatus.OK);
+    }
+}
